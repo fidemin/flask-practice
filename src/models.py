d3e63721465020327d9fb6b91dcdefef518216b0
Yunhong Min <yunhongmin00@gmail.com>
2024-04-14 16:13:57 +0900

    other models added

--- src/models.py
+++ src/models.py
@@ -4,3 +4,20 @@ from .app import db
 class User(db.Model):
     email = db.Column(db.String, primary_key=True, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+
+class Employee(db.Model):
+    employee_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+
+
+class Department(db.Model):
+    department_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+
+
+class Project(db.Model):
+    project_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
