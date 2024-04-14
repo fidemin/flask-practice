77bf9e0998c1e56fc49c9ef036c517e396bbb461
Yunhong Min <yunhongmin00@gmail.com>
2024-04-14 15:56:30 +0900

    Flask SQLAlchemy init and migration

--- src/app.py
+++ src/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!";
+    return "Hello World!"
+
+
+@app.route("/square/<int:number>")
+def square(number):
+    return str(number ** 2)
 
 
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
