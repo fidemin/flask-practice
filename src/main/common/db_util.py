46dc5da72804c97efd9d267d69786302a899f445
Yunhong Min <yunhongmin00@gmail.com>
2024-05-28 23:28:40 +0900

    transaction annotation

--- src/main/common/db_util.py
+++ src/main/common/db_util.py
@@ -1,24 +1,31 @@
+import logging
 import uuid
+from contextlib import ContextDecorator
 
 from src.main.app import db
 
-DB_SESSION_TRANSACTION_ID = "_transaction_id"
+DB_SESSION_TRANSACTION_ID = "_local_transaction_id"
+
+logger = logging.getLogger(__name__)
 
 
-class transaction:
+class transaction(ContextDecorator):
     def __enter__(self):
         self._transaction_id = str(uuid.uuid4())
-        if not getattr(db.session, DB_SESSION_TRANSACTION_ID, None):
-            setattr(db.session, DB_SESSION_TRANSACTION_ID, self._transaction_id)
+        if not getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None):
+            setattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, self._transaction_id)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if getattr(db.session, DB_SESSION_TRANSACTION_ID) != self._transaction_id:
+        if getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID) != self._transaction_id:
             return
         try:
             if exc_type is not None:
                 db.session.rollback()
             else:
                 db.session.commit()
+        except Exception as e:
+            db.session.close()
+            raise e
         finally:
-            delattr(db.session, DB_SESSION_TRANSACTION_ID)
+            delattr(db.session._proxied, DB_SESSION_TRANSACTION_ID)
--- src/tests/test_db_util.py
+++ src/tests/test_db_util.py
@@ -19,43 +19,44 @@ def temporary_table():
     )
 
     engine = create_engine(db.session.get_bind().engine.url)
-    metadata.create_all(engine)
+    try:
+        metadata.create_all(engine)
+        yield table
+    finally:
+        metadata.drop_all(engine)
 
-    yield table
 
-    metadata.drop_all(engine)
+@pytest.fixture
+def session_close():
+    yield
+    db.session.close()
 
 
 @pytest.mark.usefixtures("setup_flask_app")
-def test_transaction__commit(temporary_table):
+def test_transaction__commit(temporary_table, session_close):
     with transaction():
         db.session.execute(temporary_table.insert().values(name="test"))
 
-    db.session.rollback()
-    try:
-        actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
-    finally:
-        db.session.close()
+    db.session.rollback()  # to check all transaction is commited
+
+    actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
     assert actual.id == 1
     assert actual.name == "test"
 
 
 @pytest.mark.usefixtures("setup_flask_app")
-def test_transaction__rollback(temporary_table):
+def test_transaction__rollback(temporary_table, session_close):
     with pytest.raises(Exception):
         with transaction():
             db.session.execute(temporary_table.insert().values(name="test"))
             raise Exception("rollback")
 
-    try:
-        actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
-    finally:
-        db.session.close()
+    actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
     assert actual is None
 
 
 @pytest.mark.usefixtures("setup_flask_app")
-def test_transaction__commit_nested_call_count_check(temporary_table):
+def test_transaction__commit_nested_call_count_check(temporary_table, session_close):
     with patch.object(db.session, "commit") as mock_commit:
         with transaction():
             db.session.execute(temporary_table.insert().values(name="test1"))
@@ -67,30 +68,132 @@ def test_transaction__commit_nested_call_count_check(temporary_table):
         with transaction():
             db.session.execute(temporary_table.insert().values(name="test4"))
 
-    db.session.close()
     assert mock_commit.call_count == 2
 
 
 @pytest.mark.usefixtures("setup_flask_app")
-def test_transaction__commit_nested_check_finally_commited(temporary_table):
-    with transaction():
+def test_transaction__commit_nested_check_finally_commited(temporary_table, session_close):
+    with transaction() as first_transaction:
         db.session.execute(temporary_table.insert().values(name="test1"))
         with transaction():
             db.session.execute(temporary_table.insert().values(name="test2"))
             with transaction():
                 db.session.execute(temporary_table.insert().values(name="test3"))
+                assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is not None
+                assert first_transaction._transaction_id == db.session._proxied._local_transaction_id
 
     actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
     assert len(actual_list) == 3
-    assert getattr(db.session, DB_SESSION_TRANSACTION_ID, None) is None
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
 
     with transaction():
         db.session.execute(temporary_table.insert().values(name="test4"))
 
     db.session.rollback()  # to check all transaction is commited
-    try:
-        actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
-    finally:
-        db.session.close()
+    actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
     assert len(actual_list) == 4
-    assert getattr(db.session, DB_SESSION_TRANSACTION_ID, None) is None
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
+
+
+@transaction()
+def commit_func(table: Table):
+    db.session.execute(table.insert().values(name="test"))
+
+
+@transaction()
+def commit_func1(table: Table):
+    db.session.execute(table.insert().values(name="test1"))
+    commit_func2(table)
+
+
+@transaction()
+def commit_func2(table: Table):
+    db.session.execute(table.insert().values(name="test2"))
+    commit_func3(table)
+
+
+@transaction()
+def commit_func3(table: Table):
+    db.session.execute(table.insert().values(name="test3"))
+
+
+@transaction()
+def rollback_func1(table: Table):
+    db.session.execute(table.insert().values(name="test"))
+    raise Exception("rollback")
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_decorator__commit(temporary_table, session_close):
+    commit_func(temporary_table)
+
+    db.session.rollback()  # to check all transaction is commited
+    actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
+    assert actual.id == 1
+    assert actual.name == "test"
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_decorator__rollback(temporary_table, session_close):
+    with pytest.raises(Exception):
+        rollback_func1(temporary_table)
+
+    actual = db.session.execute(temporary_table.select().filter_by(name="test")).fetchone()
+    assert actual is None
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_decorator__commit_nested_call_count_check(temporary_table, session_close):
+    with patch.object(db.session, "commit") as mock_commit:
+        commit_func1(temporary_table)
+        commit_func(temporary_table)
+    assert mock_commit.call_count == 2
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_decorator__commit_nested_check_finally_commited(temporary_table, session_close):
+    commit_func1(temporary_table)
+
+    actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
+    assert len(actual_list) == 3
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
+
+    commit_func(temporary_table)
+
+    db.session.rollback()  # to check all transaction is commited
+
+    actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
+    assert len(actual_list) == 4
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_context_decorator_mixed__commit_nested_call_count_check(temporary_table, session_close):
+    with patch.object(db.session, "commit") as mock_commit:
+        with transaction() as first_transaction:
+            db.session.execute(temporary_table.insert().values(name="test"))
+            commit_func1(temporary_table)
+
+        commit_func1(temporary_table)
+
+    assert mock_commit.call_count == 2
+
+
+@pytest.mark.usefixtures("setup_flask_app")
+def test_transaction_context_decorator_mixed_finally_commited(temporary_table, session_close):
+    with transaction() as first_transaction:
+        db.session.execute(temporary_table.insert().values(name="test"))
+        commit_func1(temporary_table)
+        assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) == first_transaction._transaction_id
+
+    actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
+    assert len(actual_list) == 4
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
+
+    commit_func(temporary_table)
+
+    db.session.rollback()  # to check all transaction is commited
+
+    actual_list = db.session.execute(temporary_table.select().order_by('id')).fetchall()
+    assert len(actual_list) == 5
+    assert getattr(db.session._proxied, DB_SESSION_TRANSACTION_ID, None) is None
