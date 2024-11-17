f66e798f5e0c87bcc19f5f78803781a5f7bf3fb8
Yunhong Min <yunhongmin00@gmail.com>
2024-11-18 00:33:38 +0900

    optimization with fetchall and fetchmany

--- src/main/operators/fileserver.py
+++ src/main/operators/fileserver.py
@@ -30,25 +30,36 @@ def get_table_columns(schema_name, table_name):
 
 
 def table_rows_fetcher(schema_name: str, table_name: str, row_chunk_size):
-    # NOTE: Do not use fetchmany or yield_per for streaming fetching. It stores all the rows in memory.
+    # NOTE: Do not use fetchmany or yield_per for streaming fetching solely.
+    #   It stores all the rows of query result in memory Use it with offset/limit for memory optimization.
     sql_str = f"SELECT * FROM {schema_name}.{table_name} order by id offset :offset limit :limit"
     offset = 0
 
-    query = db.session.execute(text(sql_str), {"offset": offset, "limit": row_chunk_size})
-    while rows := query.fetchall():
-        yield rows
-        offset += row_chunk_size
-        query = db.session.execute(text(sql_str), {"offset": offset, "limit": row_chunk_size})
+    fetch_size = 100000
+    if row_chunk_size > fetch_size:
+        fetch_size = row_chunk_size
+
+    query = db.session.execute(text(sql_str), {"offset": offset, "limit": fetch_size})
+    rows = query.fetchmany(fetch_size)
+    while rows:
+        while rows:
+            yield rows
+            rows = query.fetchmany(row_chunk_size)
+
+        offset += fetch_size
+        query = db.session.execute(text(sql_str), {"offset": offset, "limit": fetch_size})
+        rows = query.fetchmany(row_chunk_size)
 
 
-def table_data_generator(schema_name: str, table_name: str, chunk_size):
+def table_data_generator(schema_name: str, table_name: str, chunk_size, is_chunk_size_minimum=False):
     with StringIO() as string_buffer:
         byte = b''
         writer = csv.writer(string_buffer)
         columns = get_table_columns(schema_name, table_name)
         writer.writerow(columns)
+        row_chunk_size = 1000
 
-        for rows in table_rows_fetcher(schema_name, table_name, 50000):
+        for rows in table_rows_fetcher(schema_name, table_name, row_chunk_size):
             writer.writerows(rows)
             byte += string_buffer.getvalue().encode('utf-8')
 
@@ -58,23 +69,31 @@ def table_data_generator(schema_name: str, table_name: str, chunk_size):
             # logger.info(f"buffer size: {len(byte)} bytes before yield")
             while len(byte) >= chunk_size:
                 # logger.info(f"buffer size: {len(byte)} bytes")
-                yield byte[:chunk_size]
-                byte = byte[chunk_size:]
+                if is_chunk_size_minimum:
+                    yield byte
+                    byte = b''
+                else:
+                    yield byte[:chunk_size]
+                    byte = byte[chunk_size:]
 
         if len(byte) > 0:
             yield byte
 
 
 class TableIO:
-    def __init__(self, schema_name: str, table_name: str):
+    def __init__(self, schema_name: str, table_name: str, is_chunk_size_minimum=False):
         self.generator = None
         self.schema_name = schema_name
         self.table_name = table_name
-        self.row_chunk_size = 50000
+        self.is_chunk_size_minimum = is_chunk_size_minimum
 
     def read(self, size_: int):
         if self.generator is None:
-            self.generator = table_data_generator(self.schema_name, self.table_name, size_)
+            self.generator = table_data_generator(
+                self.schema_name,
+                self.table_name,
+                size_,
+                is_chunk_size_minimum=self.is_chunk_size_minimum)
 
         try:
             print_memory_usage()
@@ -98,7 +117,6 @@ class UploadCSVFromTableOperator(Operator):
 
     @profile
     def execute(self):
-        # memory leak in multipart
         logger.info(f"Upload csv from table: {self.table_name}")
         query = f"SELECT * FROM {self.schema}.{self.table_name} order by id"
 
@@ -107,6 +125,7 @@ class UploadCSVFromTableOperator(Operator):
         with MultipartUploader(self.fileserver_client, csv_name) as uploader:
             first_chunk = True
             with BytesIO() as buffer:
+                # memory leak in read_sql_query
                 for chunk in pd.read_sql_query(query, chunksize=50000, con=db.engine):
                     logger.info(f"chunk memory use: {chunk.memory_usage().sum()}")
                     with StringIO() as string_buffer:
@@ -134,7 +153,7 @@ class UploadCSVFromTableOperator(Operator):
         csv_name = f"{self.table_name}.csv"
 
         with MultipartUploader(self.fileserver_client, csv_name) as uploader:
-            table_io = TableIO(self.schema, self.table_name)
+            table_io = TableIO(self.schema, self.table_name, is_chunk_size_minimum=True)
             while chunk := table_io.read(self.chunk_size):
                 with BytesIO(chunk) as buffer:
                     print_memory_usage("before upload part")
